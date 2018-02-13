ce256fe41fd2b2e51c33a6fdd4e53069215072e5
dimlan <dlanglitz@email.de>
2018-02-13 09:44:48 +0100

    renamed to Euromap.php

--- application/controllers/Euromap.php
+++ application/controllers/Euromap.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed' );
 
-    class EuroMap extends CI_Controller {
+    class Euromap extends CI_Controller {
 
         public function __construct(){
             parent::__construct();
