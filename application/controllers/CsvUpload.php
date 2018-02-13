9408cad941d484d7fc0126e974fc8b4df909a44d
dimlan <dlanglitz@email.de>
2018-02-13 23:04:34 +0100

    comited

--- application/controllers/CsvUpload.php
+++ application/controllers/CsvUpload.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-class CsvUpload extends CI_Controller {
+class Csvupload extends CI_Controller {
 
     public function __construct()
     {
